4750e8a61016a432ef4fd7add849571bea4f613a
Clarisse-3924 <cuwizeyimana3924@gmail.com>
2021-01-11 09:56:22 +0200

    setting onclicklistener to the exit button

--- app/src/main/java/com/example/smartbabies/facebooklogin.java
+++ app/src/main/java/com/example/smartbabies/facebooklogin.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 
         import android.content.Intent;
         import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
         import android.widget.TextView;
@@ -32,6 +33,12 @@ public class facebooklogin extends AppCompatActivity {
         login= findViewById(R.id.login);
          exit= findViewById(R.id.btn);
         callbackManager= CallbackManager.Factory.create();
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         login.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
